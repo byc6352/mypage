3ff00f2c78c457a297e1c5da41d6d6bd717751b5
byc6352 <54165564+byc6352@users.noreply.github.com>
2020-01-09 21:54:25 +0800

    Initial commit

---
+++ newPage.dpr
@@ -0,0 +1,17 @@
+program newPage;
+
+uses
+  Vcl.Forms,
+  uMain in 'uMain.pas' {fMain},
+  uFuncs in 'uFuncs.pas',
+  uConfig in 'uConfig.pas',
+  uLog in 'uLog.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TfMain, fMain);
+  Application.Run;
+end.
